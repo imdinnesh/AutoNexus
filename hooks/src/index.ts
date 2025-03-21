16d7565179f3a9b56425ac0eb7aaf6583a7167a1
imdinnesh <itsdinnesh@gmail.com>
2025-03-21 23:42:43 +0530

    add JSON parsing middleware and implement webhook trigger storage in database

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 const app = express();
-
+app.use(express.json());
 const PORT = process.env.PORT || 8000;
 
 const client=new PrismaClient();
@@ -16,6 +16,26 @@ app.get("/", (req, res) => {
 app.post("/hooks/catch/:userId/:zapId",async (req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
+    const body = req.body;
+
+    // store in db a new trigger
+    await client.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });;
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message: "Webhook received"
+    })
 
 
 });
