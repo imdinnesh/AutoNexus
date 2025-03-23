b9ab9e2515fe1a48bd06e48492a4e1b9ba864bc1
imdinnesh <itsdinnesh@gmail.com>
2025-03-23 17:15:44 +0530

    added processor

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,14 +1,42 @@
 import { PrismaClient } from "@prisma/client";
-
+import { Kafka } from "kafkajs";
 const client = new PrismaClient();
 
+const kafka = new Kafka({
+  clientId: 'outbox-processor',
+  brokers: ['localhost:9092'],
+})
+
+const TOPIC_NAME="zap-events"
+
 async function main() {
+
+  const producer = kafka.producer()
+  await producer.connect()
+
   while (true) {
     const pendinRows = await client.zapRunOutbox.findMany({
       where: {},
       take: 10, // limit the number of rows to process
     });
 
-    pendinRows.forEach((row) => {});
+    producer.send({
+      topic:TOPIC_NAME,
+      messages:pendinRows.map(row=>({
+        value:row.zapRunId
+      }))
+    })
+
+    await client.zapRunOutbox.deleteMany({
+      where:{
+        id:{
+          in:pendinRows.map(row=>row.id)
+        }
+      }
+    })
+
+    
+
+
   }
 }
