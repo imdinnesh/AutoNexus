e70a6a6a8cb254a67904806506cbacaf0d8bc028
imdinnesh <itsdinnesh@gmail.com>
2025-03-25 23:07:10 +0530

    signup route added

--- primary-backend/src/routes/user-router.ts
+++ primary-backend/src/routes/user-router.ts
@@ -1,19 +1,54 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth-middleware";
-const router=Router();
+import { SignUpSchema } from "../types/auth-types";
+import { prismaClient } from "../db";
 
-router.post("/signup",(req,res)=>{
-    console.log("Signup request received");
-})
+const router = Router();
 
-router.post("/signin",(req,res)=>{
-    console.log("Signin request received");
-})
+router.post("/signup", async (req, res) => {
+  const body = req.body;
+  const safeBody = SignUpSchema.safeParse(body);
 
-router.get("/user",authMiddleware,(req,res)=>{
-    console.log("User request received");
-})
+  if (!safeBody.success) {
+    res.status(400).json({ message: "Invalid body" });
+    return;
+  }
 
-export const userRouter=router;
+  const { username, password, name } = safeBody.data;
+
+  const UserExist = await prismaClient.user.findFirst({
+    where: {
+      email: username,
+    },
+  });
+
+  if (UserExist) {
+    res.status(409).json({ message: "User already exists" });
+    return;
+  }
+
+  await prismaClient.user.create({
+    data: {
+      email: username,
+      password,
+      name,
+    },
+  });
+
+  res.json({
+    message: "Please verify your account by checking your email",
+  });
+});
 
 
+router.post("/signin", (req, res) => {
+  console.log("Signin request received");
+  res.json({ message: "Signin endpoint" });
+});
+
+router.get("/user", authMiddleware, (req, res) => {
+  console.log("User request received");
+  res.json({ message: "User endpoint" });
+});
+
+export const userRouter = router;
