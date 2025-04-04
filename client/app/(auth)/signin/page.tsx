f9d691853f35cb1f315898e10cafb890dd1eec1c
imdinnesh <itsdinnesh@gmail.com>
2025-04-05 00:05:45 +0530

    extra routing added

--- client/app/(auth)/signin/page.tsx
+++ client/app/(auth)/signin/page.tsx
@@ -4,11 +4,16 @@ import { ExploreButton } from "@/components/Buttons/ExploreButton";
 import { GoogleButton } from "@/components/Buttons/GoogleButton";
 import { Input } from "@/components/Input";
 import { Separator } from "@/components/Separator";
+import { useRouter } from "next/navigation";
 export default function SignIn() {
+
+    const router=useRouter();
+
+
     return (
-        <div className="flex">
-            <div className="w-1/2">
-                <h1>
+        <div className="flex h-[600px] items-center justify-center px-20">
+            <div className="w-1/2 px-20">
+                <h1 className="text-3xl font-bold text-gray-900">
                     Automate across your teams
                 </h1>
                 <p>
@@ -19,7 +24,7 @@ export default function SignIn() {
                     Explore Zapier Enterprise
                 </ExploreButton>
             </div>
-            <div className="max-w-1/2">
+            <div className="max-w-1/2 px-20 border-l-2 border-gray-300">
                 <div className="w-md" >
 
                     <GoogleButton 
@@ -34,6 +39,7 @@ export default function SignIn() {
                 varient="login"
                 label="Email" 
                 size="large"
+                placeholder="Email"
                 onChange={() => { }} type="email" />
                 <AuthButton
                     onClick={() => { }}
@@ -42,6 +48,13 @@ export default function SignIn() {
                 >
                     Continue
                 </AuthButton>
+                <p className="text-sm text-gray-500 mt-2 px-4 py-2">
+                Don't have a Zapier account yet? <span
+                onClick={() => { 
+                    router.push("/signup")
+                }}
+                 className="text-violet-600 underline cursor-pointer ">Sign up</span>
+                </p>
 
             </div>
         </div>
--- client/components/Input.tsx
+++ client/components/Input.tsx
@@ -1,10 +1,12 @@
 import { CgAsterisk } from "react-icons/cg";
-export const Input=({label,type,onChange,size="small",varient="default"}:{
+export const Input=({label,type,onChange,size="small",varient="default",placeholder}:{
     label:string,
     type:"text" | "email",
     onChange:(e:any)=>void,
     size?:"small" | "large",
     varient?:"default" | "login"
+    placeholder?:string
+
 })=>{
 
     return (
@@ -17,6 +19,7 @@ export const Input=({label,type,onChange,size="small",varient="default"}:{
             <input 
             onChange={onChange}
             type={type} 
+            placeholder={placeholder}
             className={`border-2 border-gray-300 rounded-md p-2 ${size==="large"?"w-full":"w-52"} focus:outline-none focus:border-blue-500`} />
         </div>
 
